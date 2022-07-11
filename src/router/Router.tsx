6e0906c854991e3a9ae3793476b0e8c7fbb680eb
Mmy26 <yu.b1994masamura@gmail.com>
2022-07-11 17:44:08 +0900

    Headerの実装

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templetes/HeaderLayout";
 import { homeRoutes } from "./HomeRoutes";
 
 export const Router: FC = () => {
@@ -20,7 +21,7 @@ export const Router: FC = () => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
