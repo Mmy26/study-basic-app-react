6c049b8a94cef7a6a91b253484386ced80fcc3ff
Mmy26 <yu.b1994masamura@gmail.com>
2022-07-13 21:16:06 +0900

    Cardの作成

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,5 +1,17 @@
 import { FC, memo } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManegement: FC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="じゃけえ"
+          fullName="Tanaka Satoshi"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
