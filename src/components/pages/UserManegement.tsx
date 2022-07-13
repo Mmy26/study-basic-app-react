e6cd42b4898613a318541bcc2fdb9ec9ff5259da
Mmy26 <yu.b1994masamura@gmail.com>
2022-07-13 21:51:07 +0900

    APIから取得した情報に表示

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,17 +1,32 @@
-import { FC, memo } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, memo, useEffect } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 import { UserCard } from "../organisms/user/UserCard";
+import { UseAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManegement: FC = memo(() => {
+  const { getUsers, loading, users } = UseAllUsers();
+
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="じゃけえ"
-          fullName="Tanaka Satoshi"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
